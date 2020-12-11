a428d0f183cff9d87df41d5214e9e12e13720719
Longweig <lg3095@columbia.edu>
2020-12-10 20:18:04 -0500

    fix issues

--- freefree/app/secure.py
+++ freefree/app/secure.py
@@ -10,6 +10,3 @@ SECRET_KEY = 'LWJOmjpOn2r4si4nOTJD'
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_PORT = '465'
 MAIL_USE_SSL = True
-MAIL_USERNAME = '624075762@qq.com'
-MAIL_PASSWORD = 'sqtrungoxwbrbcef'
-MAIL_DEFAULT_SENDER = 'FreeFree <624075762@qq.com>'
